8edc23fe33dc901b7b585f81752945c41bf3a49b
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-28 17:00:52 -0600

    updated paths in apiRoutes

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -2,27 +2,28 @@ const router = require('express').Router();
 const saveData = require('../../db/saveData');
 
 // GET request
-router.get("../../notes", function (req, res) {
+router.get('../../public/notes.html', function (req, res) {
     saveData
-        .getNotes()
+        .retrieveNotes()
         .then(notes => res.json(notes))
         .catch(err => res.status(500).json(err));
 });
 
 // POST request
-router.post("../../notes", (req, res) => {
+router.post('../../public/notes.html', (req, res) => {
     saveData
         .addNote(req.body)
         .then((note) => res.json(note))
         .catch(err => res.status(500).json(err));
 });
 
-// BONUS: DELETE request
-router.delete("../../notes/:id", function (req, res) {
+// Bonus - DELETE request
+router.delete('../../public/notes.html/:id', function (req, res) {
     saveData
         .deleteNote(req.params.id)
         .then(() => res.json({ ok: true }))
         .catch(err => res.status(500).json(err));
 });
+
 
 module.exports = router;
