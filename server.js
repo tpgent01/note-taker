8e48cd0e524f2a74ef438aab8fafe23fde86c13f
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-28 15:33:35 -0600

    set up server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+// if client navigates to <ourhost>/api, the app will use the
+// router we set up in apiRoutes. If / is the endpoint, then
+// the router will serve back our HTML routes.
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// access front-end code in public folder
+app.use(express.static('public'));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
