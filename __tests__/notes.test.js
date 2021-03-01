e32c6b4e1002ee6a4c430d9f594837f7172c1064
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-28 19:09:09 -0600

    create note test passed

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,6 +1,18 @@
 const fs = require('fs');
 const { createNote, validateNote } = require('../lib/notes');
 const db = require('../db/db.json');
-const { jest } = require('@jest/globals');
 
 jest.mock('fs');
+
+test("Creates a new note", () => {
+    const newNote = createNote(
+        {
+            title: "First Test Note",
+            text: "blah blah"
+        },
+        db
+    );
+
+    expect(newNote.title).toBe("First Test Note");
+    expect(newNote.text).toBe("blah blah");
+});
